48022ace7c0b8e6076dbbb5726c78030f9bf90c6
Anthony Chen <chenanthony365@gmail.com>
2021-12-12 23:20:58 -0500

    improve password file loading

--- main.cpp
+++ main.cpp
@@ -4,10 +4,6 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        cout << "Creating new password file\n";
-    }
-
     string choice1;
     string choice2;
     string choice4;
@@ -15,17 +11,28 @@ int main(int argc, char *argv[]) {
     string username;
     string password;
     string notes;
+    string buffer;
     vector<Password> passwords;
     int choice3;
 
-    ifstream infile(argv[1]);
-    while (getline(infile, service)) {
-        getline(infile, username);
-        getline(infile, password);
-        getline(infile, notes);
-        passwords.push_back(Password(service, username, password, notes));
+    if (argc < 2) {
+        cout << "Creating new password file\n";
+    } else {
+        ifstream infile(argv[1]);
+        getline(infile, buffer);
+        if (buffer == "fhcreyhzvp") {
+            while (getline(infile, service)) {
+                getline(infile, username);
+                getline(infile, password);
+                getline(infile, notes);
+                passwords.push_back(Password(service, username, password, notes));
+            }
+            cout << "Password file loaded\n";
+        } else {
+            cout << "Could not load file\n";
+        }
+        infile.close();
     }
-    infile.close();
 
     while (true) {
         cout << "--------------------------------------------------------------------------------\n";
@@ -98,12 +105,14 @@ int main(int argc, char *argv[]) {
             cout << "Password file name: \n";
             cin >> choice2;
             ofstream outfile(choice2);
+            outfile << "fhcreyhzvp\n";
             for (auto password: passwords) {
                 outfile << password.getService() << "\n";
                 outfile << password.getUsername() << "\n";
                 outfile << password.getPassword() << "\n";
                 outfile << password.getNotes() << "\n";
             }
+            outfile.close();
             cout << "Password file saved\n";
         } else if (choice1 == "q") {
             return 0;
